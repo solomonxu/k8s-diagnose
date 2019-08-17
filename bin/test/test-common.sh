2be152b97eda8c8bcecc5663ae3e5325a88e7a2c
solomonxu <solomonxu@163.com>
2019-08-17 19:39:37 +0800

    Add function test_ssh_remote_trust()

--- bin/test/test-common.sh
+++ bin/test/test-common.sh
@@ -4,16 +4,26 @@
 chmod a+x ../conf/diag.conf
 . ../conf/diag.conf
 . ./common/email.sh
+. ./common/ssh.sh
 
 
 ## test notify recipients
-test_notify(){
+test_email_notify(){
+    echo "TODO: call test_email_notify"
     message_subject="K8s diagnose report"
     message_body="`date` Hosts list"
     message_attachment="/etc/hosts"
-
     notify;
 }
 
+## test ssh trust by remote hosts
+test_ssh_remote_trust(){
+    echo "TODO: call test_ssh_remote_trust"
+    ssh_remote_trust;
+}
+
 ## test notify recipients
-test_notify;
+#test_email_notify;
+
+## trust by remote hosts
+ssh_remote_trust;
