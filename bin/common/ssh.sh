d5c77b953eb85a3278918e367a51009741cd1ff0
solomonxu <solomonxu@163.com>
2019-08-21 18:53:48 +0800

    Output to Logger

--- bin/common/ssh.sh
+++ bin/common/ssh.sh
@@ -14,22 +14,26 @@ ssh_gen_rsakey()
 {
     ## check if rsa-key existed in $HOME of this user 
     if [ -n ${ID_RSA_FILE} ]; then
-        echo "`date` rsa key ${ID_RSA_FILE} has existed in local host."
+        #echo "`date` rsa key ${ID_RSA_FILE} has existed in local host."
+		logger_info "rsa key ${ID_RSA_FILE} has existed in local host."
         #return;
     fi
     ## generate local rsa-key
-	echo "`date` generate rsa-key for user ${USER} now."
-	ssh-keygen -t rsa;
+    #echo "`date` generate rsa-key for user ${USER} now."
+    logger_info "Generate rsa-key for user ${USER} now."
+    ssh-keygen -t rsa;
 }
 
 ## check and install expect
 ssh_check_expect()
 {
     ## check and install expect
-    expect_cmd=`which expect`
-    echo expect_cmd=${expect_cmd}
+    expect_cmd=$(which expect)
+    #echo expect_cmd=${expect_cmd}
+    logger_debug expect_cmd=${expect_cmd}
     if [ -z "$expect_cmd" ]; then
-       echo "expect not installed on this host. Start to install expect now..."
+       #echo "expect not installed on this host. Start to install expect now..."
+	   logger_info "expect not installed on this host. Start to install expect now..."
        yum install -y expect
     fi
 }
@@ -48,11 +52,16 @@ ssh_remote_trust()
     ssh_default_pass=`echo ${ssh_default} | awk '{print $3}'`
     ssh_default_port=`echo ${ssh_default} | awk '{print $4}'`
 	#echo "ssh_default=${ssh_default}"
-    echo "ssh_default_user=${ssh_default_user}"
-    echo "ssh_default_pass=***"
+    #echo "ssh_default_user=${ssh_default_user}"
+    #echo "ssh_default_pass=***"
     #echo "ssh_default_pass=${ssh_default_pass}"
-    echo "ssh_default_port=${ssh_default_port}"
-	echo
+    #echo "ssh_default_port=${ssh_default_port}"
+	
+	#logger_info "ssh_default=${ssh_default}"
+    logger_info "ssh_default_user=${ssh_default_user}"
+    logger_info "ssh_default_pass=***"
+    #logger_info "ssh_default_pass=${ssh_default_pass}"
+    logger_info "ssh_default_port=${ssh_default_port}"	
 	
     ## loop for hosts list
     hosts_auth=`cat ${HOSTS_AUTH_FILE} | grep -v "default" | grep -v "\[" | grep -v "\]" | grep -v "#" | awk 'BEGIN{OFS=","}{print $1,$2,$3,$4}'`
@@ -64,16 +73,20 @@ ssh_remote_trust()
             continue;
         fi
         COUNT=`expr $COUNT + 1`
-        echo "remote host No. ${COUNT}:"
-		echo "--------------------"
+        #echo "remote host No. ${COUNT}:"
+		#echo "--------------------"
+        logger_info "remote host No. ${COUNT}:"
+		logger_info "--------------------"
         ## get ssh connect's params from file line
         #echo "host_line=${host_line}"
+		#logger_info "host_line=${host_line}"
         SSH_HOST=`echo ${host_line} | awk -F, '{print $1}'`
 		SSH_USER=`echo ${host_line} | awk -F, '{print $2}'`
 		SSH_PASS=`echo ${host_line} | awk -F, '{print $3}'`
 		SSH_PORT=`echo ${host_line} | awk -F, '{print $4}'`
         if [ -z "${SSH_HOST}" ]; then
-            echo "empty SSH_HOST"
+            #echo "empty SSH_HOST"
+			logger_warn "empty SSH_HOST"
             continue;
         fi
 	    ## if ssh connect's params is empty, then set to default values
@@ -91,19 +104,27 @@ ssh_remote_trust()
             PARAM_SSH_PORT="-p ${SSH_PORT}"            
         fi
         ## echo final ssh connect's params
-        echo "SSH_HOST=${SSH_HOST}"
-        echo "SSH_USER=${SSH_USER}"
+        #echo "SSH_HOST=${SSH_HOST}"
+        #echo "SSH_USER=${SSH_USER}"
         #echo "SSH_PASS=${SSH_PASS}"
-		echo "SSH_PASS=***"
-        echo "SSH_PORT=${SSH_PORT}"
-        echo "PARAM_SSH_PORT=${PARAM_SSH_PORT}"
+        #echo "SSH_PASS=***"
+        #echo "SSH_PORT=${SSH_PORT}"
+        #echo "PARAM_SSH_PORT=${PARAM_SSH_PORT}"
+
+        logger_info "SSH_HOST=${SSH_HOST}"
+        logger_info "SSH_USER=${SSH_USER}"
+        logger_info "SSH_PASS=***"
+        logger_info "SSH_PORT=${SSH_PORT}"
+        logger_info "PARAM_SSH_PORT=${PARAM_SSH_PORT}"
+		
 		#export SSH_HOST=${SSH_HOST}; SSH_USER=${SSH_USER}; SSH_PASS=${SSH_PASS}; SSH_PORT=${SSH_PORT}; PARAM_SSH_PORT=${PARAM_SSH_PORT};
 		#export PARAM_SSH_PORT=${PARAM_SSH_PORT};
         ## copy public rsa-key to remote host
         #spawn ssh-copy-id -f -i ~/.ssh/id_rsa.pub ${PARAM_SSH_PORT} "${SSH_USER}"@"${SSH_HOST}"
         ./common/expect-ssh-copy-id.sh "${SSH_HOST}" "${SSH_USER}" "${SSH_PASS}" "${ID_RSA_FILE}" "${PARAM_SSH_PORT}"
     done;
-    echo "`date` All ${COUNT} remote hosts have trusted on this host."
+    #echo "`date` All ${COUNT} remote hosts have trusted on this host."
+    logger_info "All ${COUNT} remote hosts have trusted on this host."
 }
 
 ## install COMMAND on remote host
@@ -118,6 +139,6 @@ ssh_remote_exec()
     return;
 }
 
-## trust by remote hosts
+## trusted by remote hosts
 #ssh_remote_trust;
     
