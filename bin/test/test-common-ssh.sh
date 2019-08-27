2829f146c3e896b1769398ac6b39808d9db46503
solomonxu <solomonxu@163.com>
2019-08-28 02:35:33 +0800

    Add function test_ssh_remote_trust_concur()

--- bin/test/test-common-ssh.sh
+++ bin/test/test-common-ssh.sh
@@ -9,10 +9,48 @@ chmod a+x ../conf/diag.conf
 . ../conf/diag.conf
 
 ## test ssh trust by remote hosts
-test_ssh_remote_trust(){
+function test_ssh_remote_trust()
+{
     logger_info "TEST: call test_ssh_remote_trust"
     ssh_remote_trust;
 }
 
-## trust by remote hosts
-test_ssh_remote_trust;
+## test
+## enable trusting on this local host by remote hosts
+## run multi-tasks concurrently
+function test_ssh_remote_trust_concur()
+{
+    logger_info "TEST: call ssh_remote_trust_concur $@"
+    ssh_remote_trust_concur $@;
+}
+
+## test check and install COMMAND on remote host
+function test_ssh_remote_check_install
+{
+    logger_info "TEST: call ssh_remote_check_install $@"	
+    ssh_remote_check_install $@
+    _ret=${ret_value}
+    logger_info "TEST: check and install's result is: ${_ret}"
+}
+
+logger_info "TEST: ssh"
+
+## get host
+_host=192.168.1.11
+if [ $# -ge 1 ]; then
+    _host=$1
+fi
+
+## test trust by remote hosts
+#if [ $# -le 0 ]; then
+#    test_ssh_remote_trust;
+#fi
+
+## test
+## enable trusting on this local host by remote hosts
+## run multi-tasks concurrently
+test_ssh_remote_trust_concur 20
+
+## test check and install COMMAND on remote host
+test_ssh_remote_check_install ${_host} mysql
+test_ssh_remote_check_install ${_host} redis-cli redis
